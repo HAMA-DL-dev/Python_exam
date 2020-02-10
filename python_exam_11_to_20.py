8e974f550f88a8f5837ac5397b4ed85abc5eb819
런닝머신뛰는 하연이 <59410853+HAMA-coder@users.noreply.github.com>
2020-02-10 12:36:02 +0900

    파이썬 예제 100제 중 11~20

---
+++ python_exam_11_to_20.py
@@ -0,0 +1,41 @@
+# >> a = "3"; >> b = "4";>> print(a + b) 의 결과를 예상하라.
+a="3"
+b="4"
+print(a+b)
+
+a=3
+b=4
+print(a+b)
+
+def another_way():
+    a="3"
+    b="4"
+    print(int(a)+int(b))
+
+
+# >> s = "hello"; >> t = "python";두 변수를 이용하여 아래와 같이 출력하라.;hello! python
+s = "hello"
+t = "python"
+print(s,t,sep='! ')
+
+#>> print("Hi" * 3) 의 결과를 예상하라.
+print("Hi"*3)
+#화면에 '-'를 80개 출력하라.
+print('-'*80)
+#python java python java python java python java 를 출력하라.
+string="python java "
+print(string*4)
+#LG전자 주식을 10주 보유하고 있다. 금일 종가 20,000원일 경우 총 평가 금액을 화면에 출력하라.
+
+#>> a = "132" 의 타입을 판별하라.
+a="132"
+print(type(a))
+#>> num_str = "720"을 정수형으로 변환하라.
+num_str = "720"
+num=int(num_str)
+print(type(num))
+
+#num = 100 을 문자열로 변환하라.
+num=100
+n=str(num)
+print(type(n))
